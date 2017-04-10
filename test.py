58db05e3a526b91879ccccfb83e16d1e85ca7b2e
Christophe Combelles <ccomb@free.fr>
2017-04-10 22:47:12 +0200

    test for purge

--- test.py
+++ test.py
@@ -10,7 +10,7 @@ from buttervolume.plugin import VOLUMES_PATH, SNAPSHOTS_PATH
 from buttervolume.plugin import jsonloads, TEST_RECEIVE_PATH
 from datetime import datetime, timedelta
 from os.path import join
-from subprocess import check_output
+from subprocess import check_output, run
 from webtest import TestApp
 
 # check that the target dir is btrfs
@@ -419,6 +419,66 @@ class TestCase(unittest.TestCase):
         self.app.post('/VolumeDriver.Remove', json.dumps({'Name': name}))
         btrfs.Subvolume(join(SNAPSHOTS_PATH, snapshot)).delete()
         btrfs.Subvolume(join(SNAPSHOTS_PATH, volume_backup)).delete()
+
+    def test_purge(self):
+        """Check we can purge snapshots with a save pattern
+        """
+        name = 'buttervolume-test-' + uuid.uuid4().hex
+        # first run the purge without snapshots (should do nothing)
+        resp = self.app.post('/VolumeDriver.Snapshots.Purge',
+                             json.dumps({'Name': name, 'Pattern': '120'}))
+        # create 20 hourly snapshots corresponding to different times
+        path = join(VOLUMES_PATH, name)
+        self.app.post('/VolumeDriver.Create', json.dumps({'Name': name}))
+        with open(join(path, 'foobar'), 'w') as f:
+            f.write('original foobar')
+        
+        hours = [(datetime.now() - timedelta(hours=h)).isoformat()
+                 for h in range(20)]
+        for h in hours:
+            run('btrfs subvolume snapshot {} {}@{}'.format(
+                path, join(SNAPSHOTS_PATH, name), h), shell=True)
+        # run the purge with a simple save pattern (2h)
+        nb_snaps = len(os.listdir(SNAPSHOTS_PATH))
+        resp = self.app.post('/VolumeDriver.Snapshots.Purge',
+                             json.dumps({'Name': name, 'Pattern': '120'}))
+        # check we have 10 snapshots left
+        self.assertEqual(len(os.listdir(SNAPSHOTS_PATH)) = nb_snaps - 10)
+        # run the purge again and check we still have 10 snapshots
+        nb_snaps = len(os.listdir(SNAPSHOTS_PATH))
+        resp = self.app.post('/VolumeDriver.Snapshots.Purge',
+                             json.dumps({'Name': name, 'Pattern': '120'}))
+        # check we deleted 10 snapshots
+        self.assertEqual(len(os.listdir(SNAPSHOTS_PATH)) = nb_snaps)
+
+        # create 20 hourly snapshots corresponding to different times
+        hours = [(datetime.now() - timedelta(hours=h)).isoformat()
+                 for h in range(20)]
+        for h in hours:
+            run('btrfs subvolume snapshot {} {}@{}'.format(
+                path, join(SNAPSHOTS_PATH, name), h), shell=True)
+        # run the purge with a more complex save pattern (2h:4h:8h:16h)
+        nb_snaps = len(os.listdir(SNAPSHOTS_PATH))
+        resp = self.app.post('/VolumeDriver.Snapshots.Purge',
+                             json.dumps({'Name': name, 'Pattern': '120:240:480:960'}))
+        # check we deleted 15 snapshots
+        self.assertEqual(len(os.listdir(SNAPSHOTS_PATH)) = nb_snaps - 15)
+
+        # create 20 hourly snapshots corresponding to different times
+        hours = [(datetime.now() - timedelta(hours=h)).isoformat()
+                 for h in range(20)]
+        for h in hours:
+            run('btrfs subvolume snapshot {} {}@{}'.format(
+                path, join(SNAPSHOTS_PATH, name), h), shell=True)
+        # check we have an error with a non numeric pattern
+        resp = self.app.post('/VolumeDriver.Snapshots.Purge',
+                             json.dumps({'Name': name, 'Pattern': '60:plop:3000'}))
+        # run the purge with a more complex unsorted save pattern (1h:2h:3h:4h:5h)
+        nb_snaps = len(os.listdir(SNAPSHOTS_PATH))
+        resp = self.app.post('/VolumeDriver.Snapshots.Purge',
+                             json.dumps({'Name': name, 'Pattern': '60:120:300:240:180'}))
+        # check we deleted 13 snapshots
+        self.assertEqual(len(os.listdir(SNAPSHOTS_PATH)) = nb_snaps - 13)
 
 
 if __name__ == '__main__':
