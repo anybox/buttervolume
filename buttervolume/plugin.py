3fcaf3c3537b677d17b75c579c148164ed27faa9
Christophe Combelles <ccomb@free.fr>
2017-05-24 10:39:06 +0200

    revert previous commit

--- buttervolume/plugin.py
+++ buttervolume/plugin.py
@@ -358,7 +358,7 @@ def snapshots_purge():
             for i, age in enumerate(snapshots_age):
                 # if the age is outside the age_segment, delete nothing.
                 # Only 70 and 90 are inside the age_segment (60, 180)
-                if age <= age_segment[0] or age >= age_segment[1] <= max_age:
+                if age <= age_segment[0] or age >= age_segment[1] < max_age:
                     continue
                 # Now get the timeframe number of the snapshot.
                 # Ages 70 and 90 are in the same timeframe (70//60 == 90//60)
