24aaafe3ecf6858201d8bfbfaa63e8df05ac0cd2
Christophe Combelles <ccomb@free.fr>
2017-10-22 19:25:03 +0200

    fixed packaging

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ if sys.version_info < (3, 5):
 
 setup(
     name="buttervolume",
-    version="1.3",
+    version="1.3.1",
     author="Christophe Combelles",
     author_email="ccomb@anybox.fr",
     url="https://github.com/anybox/buttervolume",
