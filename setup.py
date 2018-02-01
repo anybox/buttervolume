0bc38d19c7a6ac19387a9bc1a2e9e914dfdf710a
Pierre Verkest <pverkest@anybox.fr>
2018-02-01 18:49:27 +0100

    Prepare version 1.5

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ if sys.version_info < (3, 5):
 
 setup(
     name="buttervolume",
-    version="1.4.0",
+    version="1.5.0",
     author="Christophe Combelles",
     author_email="ccomb@anybox.fr",
     url="https://github.com/anybox/buttervolume",
