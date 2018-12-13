5c6c3326925baa06f7054ea968d0d2965f6b649d
Christophe Combelles <ccomb@free.fr>
2018-12-13 16:06:52 +0100

    unpin urllib3

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ if sys.version_info < (3, 5):
 
 setup(
     name="buttervolume",
-    version="3.7dev",
+    version="3.7",
     author="Christophe Combelles",
     author_email="ccomb@anybox.fr",
     url="https://github.com/anybox/buttervolume",
@@ -23,7 +23,7 @@ setup(
     },
     install_requires=[
         "bottle",
-        "urllib3<=1.22",
+        "urllib3",
         "requests-unixsocket",
         "waitress",
         "webtest",
