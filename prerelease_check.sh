24c2e620e85f42dca03de826a3bb9bb1afedab03
Christophe Combelles <ccomb@free.fr>
2023-04-09 22:03:47 +0200

    version 3.10

--- prerelease_check.sh
+++ prerelease_check.sh
@@ -3,7 +3,7 @@
 set -e
 
 # version found in VERSION file
-VERSION=$(python -c 'print(float(open("buttervolume/VERSION").read().strip()))')
+VERSION=$(python -c 'print(open("buttervolume/VERSION").read().strip())')
 
 # last version found in the changelog
 CHANGES_VERSION=$(grep '^[0-9].*(.*)' CHANGES.rst | head -1 | cut -d' ' -f1)
@@ -13,7 +13,7 @@ LASTDATE=$(grep '^[0-9].*(.*)' CHANGES.rst | head -1 | cut -d' ' -f2 | tr -d '('
 
 
 if [ "$VERSION" == "" ]; then echo "Check version number"; exit 1; fi
-if [ "$VERSION" != "$CHANGES_VERSION" ]; then echo "Check version in VERSION and CHANGES.rst"; exit 1; fi
+if [ "$VERSION" != "$CHANGES_VERSION" ]; then echo "Check version in VERSION (=$VERSION) and CHANGES.rst (=$CHANGES_VERSION)"; exit 1; fi
 if ! date --date=$LASTDATE "+%d-%B-%Y" > /dev/null; then echo "Check the last date in the CHANGES.rst"; exit 1; fi
 if ! cat README.rst CHANGES.rst | rst2html > /dev/null; then echo "Check the rst syntax"; exit 1; fi
 echo "OK"
