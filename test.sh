7eb110bc7a5228f359ae3a03082454ae628ac009
Christophe Combelles <ccomb@free.fr>
2023-04-29 13:21:02 +0200

    Allow to test the current working tree

--- test.sh
+++ test.sh
@@ -1,24 +1,27 @@
 #!/bin/bash
 
 VERSION=$1
+rm -f buttervolume.zip
+pushd $( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd ) > /dev/null
 if [ "$VERSION" == "" ]; then
     VERSION="HEAD"
     echo "#####################"
-    echo "Testing version HEAD. You can test another version with: ./test.sh <VERSION>"
-    echo "Please not that only locally commited changes will be tested"
+    echo "Testing working directory with uncommited changes."
+    echo "You can test another version with: ./test.sh <VERSION>"
     echo "#####################"
+    git archive -o buttervolume.zip `git stash create`
 else
     echo "#####################"
     echo "Testing version $VERSION"
     echo "#####################"
+    git archive -o buttervolume.zip $VERSION
 fi
 
-pushd $( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd ) > /dev/null
-rm -f buttervolume.zip
-git archive -o buttervolume.zip $VERSION
 docker build --build-arg VERSION=$VERSION -t anybox/buttervolume_test:$VERSION . --no-cache
 test="sudo docker run -it --rm --privileged -v /var/lib/docker:/var/lib/docker -v $PWD:/usr/src/buttervolume -w /usr/src/buttervolume anybox/buttervolume_test:HEAD test"
 $test
+echo "#############################"
 echo "You can run tests again with:"
 echo "$test"
+echo "#############################"
 popd
